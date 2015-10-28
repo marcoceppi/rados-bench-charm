01155803d8ed29db9fbbffa953cc387dd5ca019c
Marco Ceppi <marco@ceppi.net>
2015-10-27 22:56:28 -0400

    lets get cephy with it

--- reactive/rados_bench.py
+++ reactive/rados_bench.py
@@ -1,5 +1,10 @@
 
-from charmhelpers.core.hookenv import status_set
+from charmhelpers.core.hookenv import (
+    status_set,
+    relation_get,
+)
+
+from charmhelpers.fetch import apt_install
 from charmhelpers.core.templating import render
 from charms.reactive import (
     when,
@@ -9,9 +14,23 @@ from charms.reactive import (
     remove_state,
 )
 
+from charms import ceph_utils as ceph
+
+
 @hook('install')
 def install():
-    pass
+    if not os.path.isdir('/etc/ceph'):
+        os.mkdir('/etc/ceph')
+
+    apt_install('ceph-common')
+
+
+@when('ceph.available')
+def configure_ceph(client):
+    ceph.configure(service=SERVICE_NAME, key=client.key(), auth=client.auth(),
+                   use_syslog=client.use_syslog())
+    status_set('active', '')
+
 
 @hook('benchmarks.required')
 def benchmark(rel):
