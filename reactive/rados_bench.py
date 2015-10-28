ec8492d7c446feababfc80118aa70e54065ade1b
Marco Ceppi <marco@ceppi.net>
2015-10-27 23:19:58 -0400

    way more robust statusing

--- reactive/rados_bench.py
+++ reactive/rados_bench.py
@@ -12,6 +12,7 @@ from charms.reactive import (
     hook,
     set_state,
     remove_state,
+    is_state,
 )
 
 from charms import ceph_utils as ceph
@@ -21,18 +22,45 @@ from charms import ceph_utils as ceph
 def install():
     if not os.path.isdir('/etc/ceph'):
         os.mkdir('/etc/ceph')
-
+    status_set('maintenance', 'installing dependencies')
     apt_install('ceph-common')
+    update_status()
+
+
+@when('ceph.connected'):
+def ceph_connected():
+    updated_status()
 
 
 @when('ceph.available')
 def configure_ceph(client):
+    status_set('maintenance', 'configuring ceph client')
     ceph.configure(service=SERVICE_NAME, key=client.key(), auth=client.auth(),
                    use_syslog=client.use_syslog())
-    status_set('active', '')
+    update_status()
+
+
+@when_not('ceph.available')
+def configure_ceph(client):
+    status_set('maintenance', 'removing ceph configuration')
+    os.remove('/etc/ceph/ceph.conf')
+    update_status()
 
 
 @hook('benchmarks.required')
 def benchmark(rel):
+    status_set('maintenance', 'sending benchmarks...')
     rel.benchmarks(['write', 'read-rand', 'read-seq'])
-    status_set('active', 'streaming metric data')
+    update_status()
+
+
+@hook('update-status')
+def update_status():
+    if not is_state('ceph.connected'):
+        return status_set('blocked', 'need to be connected to a ceph cluster')
+
+    if is_state('ceph.connected') and not is_state('ceph.available'):
+        return status_set('waiting', 'credentials from ceph')
+
+
+    status_set('active', 'ready to benchmark')
